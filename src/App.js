a4855447155b17d91e81bdb7f65fb7a79017568e
amy-creasy <72656978+amy-creasy@users.noreply.github.com>
2020-12-23 10:55:11 +0000

    Added URL for GitHub repository.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Amy Creasy and is  <a href="https://github.com/amy-creasy/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
