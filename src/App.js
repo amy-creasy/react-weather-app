af500bc22ea8035246348b581232e3066b4442e2
amy-creasy <72656978+amy-creasy@users.noreply.github.com>
2020-12-23 11:11:33 +0000

    Added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Amy Creasy and is  <a href="https://github.com/amy-creasy/react-weather-app" target="_blank">open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Amy Creasy and is  <a href="https://github.com/amy-creasy/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
