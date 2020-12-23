d8480999549c619d4d7e2ad5f6c13f8e61a5fef2
amy-creasy <72656978+amy-creasy@users.noreply.github.com>
2020-12-23 14:26:16 +0000

    Added CSS to Weather component.

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Weather.css";
+import cloudIcon from "./cloud icon.png";
+import humidityIcon from "./humidity icon.png";
+import windIcon from "./wind icon.png";
 
 export default function Weather() {
     return (
         <div className ="Weather">
-            <form>
+            <form className ="search-form">
                 <div className = "row">
                     <div className ="col-9">
-                        <input type ="search" placeholder ="Search for a city..." className="form-control"/>
-                        <p><small><a href="#">Current Location</a></small></p>
+                        <input type ="search" placeholder ="Search for a city..." className="form-control" autoFocus="on"/>
+                        <p id="current-location"><small><a href="#">Current Location</a></small></p>
                     </div>
-                    <div className ="col-3">
+                    <div className ="col-3 submit-button">
                         <input type ="submit" value ="Search" className="btn btn-light"/>
                     </div>
                 </div>
@@ -21,7 +24,7 @@ export default function Weather() {
             </div> 
             <div class="current-weather">
                 <div class="current-weather-icon-container">
-                    <img src="01d.png" id="current-weather-icon" alt="" />
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" id="current-weather-icon" alt="" />
                  </div>
                 <div class="current-weather-content-container">
                     <div class="current-temperature"><span id="temperature-value">20</span><span class="units"><a href="#" class="active" id="celsius-link">°C</a> | <a href="#" class="inactive" id="fahrenheit-link">°F</a></span> </div>
@@ -34,7 +37,7 @@ export default function Weather() {
                         Clouds
                     </div>
                     <div class="image">
-                        <img src="./cloud icon.png" alt=""/>
+                        <img src={cloudIcon} alt=""/>
                     </div>
                     <div class="value" id="cloudiness-value">
                         20%
@@ -45,7 +48,7 @@ export default function Weather() {
                         Humidity
                     </div>
                     <div class="image">
-                        <img src="./humidity icon.png" alt=""/>
+                        <img src={humidityIcon} alt=""/>
                     </div>
                     <div class="value" id="humidity-value">
                         80%
@@ -56,7 +59,7 @@ export default function Weather() {
                         Wind
                     </div>
                     <div class="image">
-                        <img src="./wind icon.png" alt=""/>
+                        <img src={windIcon} alt=""/>
                     </div>
                     <div class="value" id="wind-value">
                         40mph
